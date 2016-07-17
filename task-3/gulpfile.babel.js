ad2b9e83ff07968340a8022e84006eb673946863
4esnog <4esnog96@gmail.com>
2016-07-17 11:25:19 +0300

    First commit for task-3

---
+++ task-3/gulpfile.babel.js
@@ -0,0 +1,75 @@
+import gulp from 'gulp';
+import concat from 'gulp-concat';
+import postcss from 'gulp-postcss';
+import sourcemaps from 'gulp-sourcemaps';
+import uglify from 'gulp-uglify';
+
+import cssImport from 'postcss-import';
+import size from 'postcss-size';
+import uncss from 'postcss-uncss';
+import cssnext from 'postcss-cssnext';
+import mqpacker from 'css-mqpacker';
+import cssnano from 'cssnano';
+import stylelint from 'stylelint';
+// import sugarss from 'sugarss';
+
+import eslint from 'eslint';
+import bs from 'browser-sync';
+
+
+gulp.task('html', () => {
+	return gulp.src('src/**/*.html')
+	.pipe(gulp.dest('public/'))
+	.pipe(bs.stream());
+});
+
+gulp.task('css', () => {
+
+	const processors = [
+		// cssImport({
+		// 	plugins: [
+		// 		uncss({
+		// 			html: ['src/**/*.html']
+		// 		})
+		// 	]
+		// }),
+		cssnext({
+     	autoprefixer: ['ie >= 10', '> 2% in RU']
+  	}),
+		size,
+		mqpacker,
+		cssnano({
+			autoprefixer: false
+		})
+	];
+
+	return gulp.src('src/**/*.css')
+	.pipe(sourcemaps.init())
+	.pipe(postcss(processors))
+	.pipe(sourcemaps.write('.'))
+	.pipe(gulp.dest('public/'))
+	.pipe(bs.stream());
+});
+
+gulp.task('js', () => {
+	return gulp.src('src/**/*.js')
+	.pipe(sourcemaps.init())
+	.pipe(uglify())
+	.pipe(sourcemaps.write('.'))
+	.pipe(gulp.dest('public/'))
+});
+
+gulp.task('watch', () => {
+	gulp.watch('src/**/*.html', gulp.series('html'));
+	gulp.watch('src/**/*.css', gulp.series('css'));
+	gulp.watch('src/**/*.js', gulp.series('js'));
+});
+
+gulp.task('server', () => {
+	return bs.init({
+		server: 'public/',
+		open: true
+	})
+});
+
+gulp.task('default', gulp.series('html', 'css', gulp.parallel('server', 'watch')));
